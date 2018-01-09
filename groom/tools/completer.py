3c2731146fc13e3de1c42117d285596666a63709
Bruno Dupuis <lisael@lisael.org>
2018-01-09 08:54:35 +0100

    added completer package

---
+++ groom/tools/completer.py
@@ -0,0 +1,55 @@
+import os
+
+from groom.utils import find_pony_stdlib_path
+
+
+class Loader:
+    """
+    Resolve imports and load scopes
+    """
+    def __init__(self, uses=None, path=None, ponypath=None):
+        self.path = path
+        self.set_ponypath(ponypath)
+        self.uses = uses if uses is not None else []
+
+    def get_ponypath(self, ponypath):
+        stdlib = None
+        # normalize to a list of path
+        if ponypath is not None:
+            if isinstance(ponypath, str):
+                ponypath = [ponypath]
+        else:
+            ponypath = []
+
+        # check if the stdlib is in the ponypath
+        for path in ponypath:
+            try:
+                dirs = os.listdir(path)
+                # check for a couple of known packages
+                if "builtin" in dirs:
+                    if "builtin_test" in dirs:
+                        if "capsicum" in dirs:
+                            stdlib = path
+                            break
+            except OSError:
+                continue
+
+        # if stdlib wasn't found, try to find it ourselves.
+        if stdlib is None:
+            try:
+                ponypath = [find_pony_stdlib_path()] + ponypath
+            except FileNotFoundError:
+                pass
+
+        if path is not None:
+            self.ponypath.append(os.path.dirname(path))
+        self.ponypath = ponypath
+        self.stdlib_path = stdlib
+
+
+def complete(src, pos, path=None, ponypath=None):
+    """
+    Generate suggestions based on current source and position
+    """
+    loader = Loader()
+
